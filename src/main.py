f5b75e8b2b9b0f0b5f8853fcfa21c8c6049438f4
Michael Vega <48607909+michaelvega@users.noreply.github.com>
2022-02-02 20:04:56 -0500

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from lib import *
+
+
+def main():
+    original_image = cv.imread('stockimage3.jpg')
+
+    grayscale_image = cv.cvtColor(original_image, cv.COLOR_BGR2GRAY)
+
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+
+    detected_faces = face_cascade.detectMultiScale(grayscale_image, 1.2)
+    # 1.3 originally
+
+    for (column, row, width, height) in detected_faces:
+        cv.rectangle(
+            original_image,
+            (column, row),
+            (column + width, row + height),
+            (255, 0, 0),
+            4
+        )
+
+    small = cv.resize(original_image, (0, 0), fx=0.3, fy=0.3)
+
+    cv.imshow('Image', original_image)  # original_image or small
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
